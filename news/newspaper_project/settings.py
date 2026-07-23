79d6a6d9e8df4825e3899dd0f0bfa203a7082b95
Liannaka Dadi <liannaka.dadi10@gmail.com>
2026-07-23 22:59:58 +0530

    Bootstrap

--- news/newspaper_project/settings.py
+++ news/newspaper_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "users.apps.UsersConfig", #new
+    'pages.apps.PagesConfig', # new
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
