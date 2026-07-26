f19f0ab019dd7a1295aefa7dc571ec0b8aa456cd
Liannaka Dadi <liannaka.dadi10@gmail.com>
2026-07-26 23:51:47 +0530

    comments on articles list

--- news/articles/admin.py
+++ news/articles/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article
-admin.site.register(Article)
+from .models import Article, Comment # new
+class CommentInline(admin.StackedInline): # new
+    model = Comment
+# class CommentInline(admin.TabularInline): # new
+#     model = Comment
+class ArticleAdmin(admin.ModelAdmin): # new
+    inlines = [
+    CommentInline,
+    ]
+admin.site.register(Article, ArticleAdmin) # new
+admin.site.register(Comment) # new
